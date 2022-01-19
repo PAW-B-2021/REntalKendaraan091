e5b98d6d807b9c462c9405de30f2619e343c234b
Elfandira Ade <elfandira.ade.ft19@mail.umy.ac.id>
2022-01-19 10:47:37 +0700

    Add project files.

---
+++ REntalKendaraan/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REntalKendaraan.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Peminjaman = new HashSet<Peminjaman>();
+        }
+
+        public int IdCustomer { get; set; }
+        public string NamaCustomer { get; set; }
+        public byte[] Nik { get; set; }
+        public byte[] Alamat { get; set; }
+        public string NoHp { get; set; }
+        public int? IdGender { get; set; }
+
+        public Gender IdGenderNavigation { get; set; }
+        public ICollection<Peminjaman> Peminjaman { get; set; }
+    }
+}
---
+++ REntalKendaraan/Models/Pengembalian.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REntalKendaraan.Models
+{
+    public partial class Pengembalian
+    {
+        public int IdPengembalian { get; set; }
+        public DateTime? TglPengembalian { get; set; }
+        public int? IdPeminjaman { get; set; }
+        public int? IdKondisi { get; set; }
+        public int? Biaya { get; set; }
+
+        public KondisiKendaraan IdKondisiNavigation { get; set; }
+        public Peminjaman IdPeminjamanNavigation { get; set; }
+    }
+}
